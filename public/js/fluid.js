cfe97c9b6fb26fbe987ead6612a5dcbbd0639863
Kris Hedges <KrisHedges@gmail.com>
2012-02-06 17:46:02 -0500

    Updated Downstream from latest EP Also made some changes to the FLuidity Homepage

--- public/js/fluid.js
+++ public/js/fluid.js
@@ -1,49 +1,73 @@
 $(document).ready( function(){
-  var topsquares = $('.topbar .stage');
-  var headersquares = $("header .stage");
-  var squares = "<div class='square-x'><div class='square-y'><div class='square'></div></div></div><div class='square2-x'><div class='square2-y'><div class='square2'></div></div></div>";
-  var d = new Date();                   
-  var year = d.getFullYear();
-  $(".copyright").append(" &copy; " + year);
-  $('a[href*=#]').click( function(){
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
-       var $target = $(this.hash);
-       $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-       if ($target.length) {
-         var targetOffset = $target.offset().top - 62;
-         $('html,body').animate({scrollTop: targetOffset}, 150);
-         return false;
-       };
+  var d, year, $target, squares, topsquares, headersquares;
+
+  addYearToCopyright = function() {
+    var d = new Date();                   
+    var year = d.getFullYear();
+    $(".copyright").append(" &copy; " + year);
+  };
+
+  topbarMenu = function() {
+    offsetTargetScrollForTopbar = function(){
+      $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+           var $target = $(this.hash);
+           $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+           if ($target.length) {
+             var targetOffset = $target.offset().top - 62;
+             $('html,body').animate({scrollTop: targetOffset}, 150);
+             return false;
+           };
+        };  
+      });
     };
-  });
-
-  firesquares = function() {
-    topsquares.append(squares);
-    headersquares.append(squares);
+    $(".topbar h1").click( function(){
+       $('html, body').animate({ scrollTop: 0 }, 150);                             
+    });
+    offsetTargetScrollForTopbar();
   };
 
-  var stopIt = function(){
-    setTimeout("clearInterval(fireInterval)", 3000);
-  };
-  stopIt();
+  helix = function(){
+    var topsquares = $('#topbar-stage');
+    var headersquares = $("#header-stage");
+    var squares = "<div class='square-x'><div class='square-y'><div class='square'></div></div></div><div class='square2-x'><div class='square2-y'><div class='square2'></div></div></div>";
 
-  fireAway = function(){
-    fireInterval = setInterval("firesquares()", 100);
+    createHelixes = function(){
+      firesquares = function(){
+        topsquares.append(squares);
+        headersquares.append(squares);
+      };
+      stopIt = function(){
+        setTimeout("clearInterval(fireInterval)", 3100);
+      };
+      fireAway = function(){
+        fireInterval = setInterval("firesquares()", 100);
+      };
+      fireAway();
+      stopIt();
+    };
+    createHelixes();
+    
+    moveHelixOnScroll =  function(){
+      $(window).scroll(function (){
+       var y = $(window).scrollTop();
+       if(y < 110){
+           topsquares.removeClass('shown');
+        };
+       if(y > 110){
+           topsquares.addClass('shown');
+        };
+      });
+    };
+    moveHelixOnScroll();
   };
-  fireAway();
-                   
-  $(window).scroll( function (){
-     var y = $(window).scrollTop();
-     if(y < 110){
-         topsquares.removeClass('shown');
-      }
-     if(y > 110){
-         topsquares.addClass('shown');
-      }
-   });
+  
+  init = function() {
+    setTimeout("helix()",1500);
+    topbarMenu();                     
+    addYearToCopyright();             
+  };
+  init();                     
 
-  $(".topbar h1").click( function(){
-     $('html, body').animate({ scrollTop: 0 }, 150);                             
-  });
 
 });
